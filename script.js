583c16d42d906246b73c6e2634378368fe35da8a
Tam Byrne <mercadier122@gmail.com>
2019-12-08 01:33:57 +0000

    js

--- script.js
+++ script.js
@@ -1,18 +1,9 @@
 'use strict'
 
-//Weather API
-// const chez = $('#chez').val();
- const url = 'https://cors-anywhere.herokuapp.com/https://samples.openweathermap.org/data/2.5/weather?q='
-                 + chez + '&appid=2b66302e97ef0cc8c1f7bf359ac95715';
-
-
-//Maps API
-// const city = $('#user-input').val();
-const locUrl = 'https://api.opencagedata.com/geocode/v1/json?q=' + city + '&key=96d46d5ed9764000afb371a04ad3ef5b';
-
 function getWeather(chez) {
-    debugger;
-    fetch(url, chez)
+    const url = 'https://cors-anywhere.herokuapp.com/https://samples.openweathermap.org/data/2.5/weather?q='
+                 + chez + '&appid=2b66302e97ef0cc8c1f7bf359ac95715';
+    fetch(url)
     .then(response => {
         if(!response.ok){
             throw new Error(response.statusText)
@@ -40,7 +31,8 @@ function showResult(responseJson) {
 }
 
 function getLocation(city) {
-    fetch(locUrl, city) 
+    const locUrl = 'https://api.opencagedata.com/geocode/v1/json?q=' + city + '&key=96d46d5ed9764000afb371a04ad3ef5b';
+    fetch(locUrl) 
     .then(response => {
         if(!response.ok) {
             throw new Error(response.statusText)
@@ -51,7 +43,6 @@ function getLocation(city) {
     .catch(err => {
         alert(err.message)
     })
-    console.log(responseJson);
 }
 
 function locResult() {
@@ -65,8 +56,6 @@ function weatherClick() {
         e.preventDefault();
         console.log(e);
         const chez = $('#chez').val();
-        // const url = `https://cors-anywhere.herokuapp.com/https://samples.openweathermap.org/data/2.5/weather?q=
-                        // ${chez}&appid=2b66302e97ef0cc8c1f7bf359ac95715`;
         getWeather(chez);
     })
 }
