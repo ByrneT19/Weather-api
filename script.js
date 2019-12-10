d30a5791f378f84dfa58037691a7b583a2a528f6
Tam Byrne <mercadier122@gmail.com>
2019-12-10 00:00:03 +0000

    js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 'use strict'
                 
 function getWeather(CORDS) {
+    console.log(CORDS);
     const url = `https://api.openweathermap.org/data/2.5/find?lat=${CORDS[0]}&lon=${CORDS[1]}&units=imperial&appid=2b66302e97ef0cc8c1f7bf359ac95715`;
     fetch(url)
     .then(response => {
@@ -48,11 +49,22 @@ function getLocation(wgs, country) {
     })
 }
 
-function locResult(results, _type, responseJson) {
-    console.log(responseJson);
-    const CORDS = [`${results.geometry.lat}`,`${results.geometry.lng}`];//use push method here with two let variables?
+function locResult(results, _type, responseJson) { //Object.keys method?
+    console.log(results);
+    /*let i = { //trying to create something that will assign the index to the result that has _type
+    [_type]: value,
+  }*/
+    if(Object.keys == responseJson.results.components._type){
+        return(Object.keys(results.components._type[i]));
+    }else{
+        alert('Sorry something went wrong please try-again (tip: make sure the post code is correct)');
+    }
+    const CORDS = [`${results[i].geometry.lat}`,`${results[i].geometry.lng}`];//use push method here with two let variables?
     getWeather(CORDS);
+    console.log(CORDS);
 }
+
+
 
 /*function weatherClick() {
     $('.weather-form').on('submit', function(e) {
